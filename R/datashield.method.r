f65619304c4593b1be4e26817deb9716212ddc46
Yannick Marcon <yannick.marcon@obiba.org>
2021-07-05 15:20:06 +0000

    version 1.3.0

--- R/datashield.method.r
+++ R/datashield.method.r
@@ -1,4 +1,18 @@
 
+#' List Datashield profiles
+#' 
+#' Get available Datashield profiles.
+#' 
+#' @param opal Opal object.
+#' @keywords internal
+.datashield.profiles <- function(opal) {
+  if (opal.version_compare(opal, "4.2")<0)
+    list(available = "default", current = "default")
+  else
+    list(available = sapply(opalr::opal.get(opal, "datashield", "profiles"), function(p) p$name),
+        current = ifelse(nchar(opal$profile) == 0, "default", opal$profile))
+}
+
 #' List Datashield methods
 #' 
 #' Get available Datashield methods of a given type.
